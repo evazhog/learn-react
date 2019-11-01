1c02f14e636860b3f40e6e1624f8b0967b8c32c0
evazhog <evanzhong@ucla.edu>
2019-10-31 19:33:05 -0700

    implemented strike-through and delete button

--- src/checklist.js
+++ src/checklist.js
@@ -5,18 +5,29 @@ let styles = {}
 function Check(props) {
   return (
     <div style={{display:'flex'}}>
-      <input type="checkbox" onChange={() => {props.whenChecked()}}/>
-      <div className={"checkbox-text"}>{props.text}</div>
+      <input type="checkbox" onChange={() => {props.whenChecked()}} disabled={false /*props.isChecked use this is we don't want toggle ability*/}/>
+      <div className={"checkbox-text"} style={props.isChecked ? {textDecoration: "line-through",} : null}>{props.text}</div>
+      {
+        props.isChecked && <input type={"submit"} onClick={()=>{props.deleteComponent()}} value={'Delete'} className={"delete"}/>
+      }
     </div>
-  )
+  );
 }
 
 export function CheckList() {
   const [inputText, setInputText] = useState('');
   const [toDos, setToDos] = useState({});
   
-  // When a To Do item is checked, remove it
+  // When a To Do item is checked, toggle strike out
   const whenChecked = (key) => {
+    let t = Object.assign({}, toDos);
+    let oldFlag = t[key].isChecked;
+    t[key].isChecked = !oldFlag; //in order to toggle
+    setToDos(t);
+  }
+
+  //This deletes the component
+  const deleteComponent = (key) => {
     let t = Object.assign({}, toDos);
     delete t[key];
     setToDos(t);
@@ -25,19 +36,21 @@ export function CheckList() {
   // Render-ing functions
   const renderCheck = (props) => {
     return (
-      <Check key={props.key} text={props.text} whenChecked={props.whenChecked}/>
+      <Check key={props.key} text={props.text} whenChecked={props.whenChecked} isChecked={props.isChecked} deleteComponent={props.deleteComponent} />
     )
   }
 
   const renderToDos = () => {
     let toDoComponents = []
     for (let key in toDos){
-      const text = toDos[key]
+      const toDo = toDos[key]
       toDoComponents.push(renderCheck(
         {
           key: key,
-          text: text,
+          text: toDo.inputText,
           whenChecked: () => { whenChecked(key) },
+          isChecked: toDo.isChecked,
+          deleteComponent: () => {deleteComponent(key)},
         }
       ))
     }
@@ -49,7 +62,10 @@ export function CheckList() {
     if (!inputText) return;
     let t = Object.assign({}, toDos);
     const currentMax = Math.max(Object.keys(t).length); // Too lazy to generate real hashes for this so we're just using numbers
-    t[currentMax + 1] = inputText;
+    t[currentMax + 1] = {
+      inputText: inputText,
+      isChecked: false,
+    };
     setToDos(t);
   }
   const onTextChange = (event) => {
